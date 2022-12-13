44b076b9cfd62dce731fc1eb18b09c1fe5377b24
AguJP <agujpa@gmail.com>
2022-12-12 18:52:10 -0800

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point, Prints alpha in lower case
+ * Return: Always 0
+ */
+
+int main(void)
+char m;
+for (m = 'a'; m <= 'z'; m++);
+{
+putchar(m);
+}
+putchar('\n');
+return (0);
