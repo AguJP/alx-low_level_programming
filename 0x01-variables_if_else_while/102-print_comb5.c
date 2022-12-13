a2f39ab9352ddead50f524876c60919f623d326c
AguJP <agujpa@gmail.com>
2022-12-12 21:35:40 -0800

    Print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,10 +14,10 @@ for (n = 0; n <= 100; n++)
 {
 if (m < n)
 	{
-	putchar(m + '0');
-	putchar((m % 10) + '0');
-	putchar(n + '0');
-	putchar((n % 10) + '0');
+	putchar((m / 10) + 48);
+	putchar((m % 10) + 48);
+	putchar((n / 10) + 48);
+	putchar((n % 10) + 48);
 	if (m == 98 || n == 99)
 	{
 	;
