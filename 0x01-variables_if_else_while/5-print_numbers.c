83c06d9ea367c37c3c8bb577b58d8b22245f7418
AguJP <agujpa@gmail.com>
2022-12-12 19:14:25 -0800

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point, Prints all single digits of base 10
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int m;
+for (m = 0; m <= 9 ; m++)
+{
+printf("%d", m);
+}
+printf("\n");
+return (0);
+}
