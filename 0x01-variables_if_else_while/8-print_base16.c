9557f2700c58fb7bd9c76d41b01e96d9b3d0a893
AguJP <agujpa@gmail.com>
2022-12-12 19:33:50 -0800

    Print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point, Prints lowercase digits of base 16 Using putchar
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int m;
+char n;
+for (m = 0; m <= 9 ; m++)
+{
+putchar((m % 10) + '0');
+}
+for (n = 'a'; n <= 'f'; n++)
+{
+putchar('n');
+}
+putchar('\n');
+return (0);
+}
