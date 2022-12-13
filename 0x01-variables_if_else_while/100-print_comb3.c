aac944067d18b86ea083ec9a1eade26703421139
AguJP <agujpa@gmail.com>
2022-12-12 20:15:28 -0800

    Print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point, Prints combination of 2 digits Using putchar
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int m;
+int n;
+for (m = 0; m <= 9 ; m++)
+{
+  for (n = 0; n <= 9; n++)
+    {
+      if (!((m == n) || (m > n)))
+	{
+	  putchar(m + '0');
+	  putchar(n + '0');
+	  if (!(m == 9 && n == 8))
+	    {
+	      putchar(',');
+	      putchar(' ');
+	    }
+	}
+    }
+ }
+putchar('\n');
+return (0);
+}
