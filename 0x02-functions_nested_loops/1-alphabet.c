33efa5ad404cb6b938e8c79d617ad9ff1f9b64f6
AguJP <agujpa@gmail.com>
2022-12-13 00:27:51 -0800

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+char A;
+for (A = 'a'; A <= 'z'; A++)
+{
+_putchar(A);
+}
+_putchar(\n);
+return (void);
+}
