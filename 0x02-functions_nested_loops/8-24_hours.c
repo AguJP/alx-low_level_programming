18f877f07186829ca0d653117e1abcbfab7bb334
AguJP <agujpa@gmail.com>
2022-12-13 20:35:26 -0800

    Jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,6 +6,7 @@
 void jack_bauer(void)
 {
 	int n;
+	int m;
 	for (n = 0; n < 60; n++)
 	{
 		for (m = 0; m < 60; m++)
@@ -15,11 +16,13 @@ void jack_bauer(void)
 				_putchar('0');
 			}
 			_putchar(n + '0');
+			_putchar(':');
 			if (m < 10)
 			{
-				_putchar(0);
+				_putchar('0');
 			}
 			_putchar(m + '0');
+			_putchar('\n');
 		}
 	}
 }
