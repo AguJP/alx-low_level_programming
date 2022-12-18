5f3cfda572b0a70e6ce0ac1d694bfe30cc3fa794
AguJP <agujpa@gmail.com>
2022-12-18 12:45:26 -0800

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - Function to print line n times
+ * @n: Number of times to print the character _
+ * Return: Always 0
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n < 1)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+}
