f9243682e17a0b88ec94355c4174d7029d88a367
AguJP <agujpa@gmail.com>
2022-12-23 01:15:33 -0800

    String N copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strncpy - Function to copy string
+ * @src: string to be copied
+ * @dest: string destination
+ * @n: maximum byte to copy
+ * Return: A pointer to the destination string @dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
+	char *p = dest;
+
+	while ((n > 0) && (*src != '\0'))
+	{
+		*p = *src;
+		p++;
+		src++;
+		n--;
+	}
+
+	*p = '\0';
+
+	return (dest);
+}
