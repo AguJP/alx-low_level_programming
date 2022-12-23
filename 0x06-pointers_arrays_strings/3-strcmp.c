b50e87f7e7b166637a99ae8fe9455d67cf3bc228
AguJP <agujpa@gmail.com>
2022-12-23 06:09:38 -0800

    String compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,15 +11,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+int i;
 
-	while ((*s1) && (*s2) && (*s1 == *s2))
-        {
-                s1++;
-                s2++;
-        }
-
-        i = (*s1 - *s2);
-
-	return (i);
+while ((*s1) && (*s2) && (*s1 == *s2))
+{
+s1++;
+s2++;
+}
+i = (*s1 - *s2);
+return (i);
 }
