15ae0b8cfa6f3ff9f628dce63e7c180beee2b3cf
AguJP <agujpa@gmail.com>
2022-12-26 14:21:08 -0800

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,9 +23,9 @@ void print_times_table(int n)
 
 				prod = num * mult;
 
-				if (prod <= 99)
+				if (prod < 100)
 					_putchar(' ');
-				if (prod <= 9)
+				if (prod < 10)
 					_putchar(' ');
 				if (prod >= 100)
 				{
@@ -33,7 +33,7 @@ void print_times_table(int n)
 					_putchar(((prod / 100) % 10) + '0');
 				}
 
-				else if (prod <= 99 && prod >= 10)
+				else if (prod < 100 && prod >= 10)
 					_putchar((prod / 10) + '0');
 				_putchar((prod % 10) + '0');
 			}
