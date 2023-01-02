91d564569f1dab8ab4377adc2cc9cecd980ca8a2
AguJP <agujpa@gmail.com>
2023-01-02 14:08:03 -0800

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,10 +12,11 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned char *memarea = s;
 	unsigned char value = b;
+	unsigned int i;
 
-	while (n--)
+	for (i = 0; i < n; i++)
 	{
-		*memarea++ = value;
+		memarea[i] = value;
 	}
 
 	return (memarea);
