9014683c90b4dbc45d20d996f83748205296c654
AguJP <agujpa@gmail.com>
2023-01-04 20:46:19 -0800

    strlen recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Function to return length of a string
+ * @s: pointer to string
+ * Return: Always 1
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i++;
+		_strlen_recursion(s + 1);
+	}
+
+	return (i);
+}
