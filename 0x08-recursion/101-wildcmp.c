1e5710890a2e4d0279ddfcba62c1bec1aa84b036
AguJP <agujpa@gmail.com>
2023-01-05 16:55:02 -0800

    wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * wildcmp - Function to compare two strings for equivalence
+ * @s1: First string
+ * @s2: Second string to be compared with s1
+ * Return: 1 if equal, otherwise 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == '\0' || *s2 == '\0')
+		return (0);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	return (0);
+}
