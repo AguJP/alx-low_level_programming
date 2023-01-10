4d16ef809f8dc86d705f6084742a7214d4e63813
AguJP <agujpa@gmail.com>
2023-01-10 02:29:09 -0800

    Multiply two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,7 +6,7 @@
  * main - Entry point of program to print product of two numbers
  * @argc: argument count
  * @argv: argument vector
- * Return: Always 0
+ * Return: 0 if true, else 1
  */
 
 int main(int argc, char *argv[])
@@ -23,7 +23,6 @@ int main(int argc, char *argv[])
 		i = atoi(argv[1]);
 		j = atoi(argv[1]);
 		printf("%d\n", i * j);
+		return (0);
 	}
-
-	return (0);
 }
