acfc138abb51875354adf794d43cbfdf8afc08b2
AguJP <agujpa@gmail.com>
2023-01-09 22:43:46 -0800

    Whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - program to print the name of a program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0
+ */
+
+int main (int argc, char *argv[])
+{
+	printf (argv[0]"\n");
+}
