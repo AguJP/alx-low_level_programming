bad0c9bbdbfd98834c73fc6424e8f98a22921822
AguJP <agujpa@gmail.com>
2023-01-10 20:43:01 -0800

    Add positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * is_valid_num - Program to check for number
+ * @str: string to be checked for number
+ * Return: Always 0
+ */
+
+int is_valid_num(char *str)
+{
+	int len = strlen(str);
+	for (int i = 0; i < len; i+++)
+	{
+		if (!isdigit(str[i]))
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+
+/**
+ * main - Entry point of program to print the name of a program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	int sum = 0;
+	for (int i =1; i < argc; i++)
+	{
+		if (!is_valid_num(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
