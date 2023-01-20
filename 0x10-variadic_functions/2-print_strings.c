e77716da805d16e8f2b0808434b7961d42a7f253
AguJP <agujpa@gmail.com>
2023-01-20 14:43:38 -0800

    Print strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - Variadic function to print strings
+ * @separator: Pointer to string used as separator
+ * @n: Number of strings supplied
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list sum;
+	unsigned int i;
+	char s;
+
+	va_start(sum, n);
+
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(sum, int);
+		if (s != NULL)
+		{	printf("%s", s);
+		}
+		else
+			printf("(nil)");
+
+		if ((i != (n - 1)) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+
+	printf("\n");
+
+	va_end(sum);
+}
