2922608cafbb515a26c11da227723b63b8d992cd
AguJP <agujpa@gmail.com>
2023-01-22 15:32:52 -0800

    Calculator

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,7 +17,7 @@ int (*get_op_func(char *s))(int, int)
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL},
+		{NULL, NULL}
 	};
 
 	while ((ops[i].op != NULL) && (*(ops[i].op) != *s))
