6fe5670f656790a5f6eccea8ce79423232170490
AguJP <agujpa@gmail.com>
2023-01-22 16:44:22 -0800

    Calculator

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -2,7 +2,7 @@
 #define CALC_H
 
 /**
- * struct op - Struct op
+ * struct op - A struct op
  * @op: The operator
  * @f: The function associated
  */
@@ -11,7 +11,7 @@ typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
-}op_t;
+} op_t;
 
 int op_add (int a, int b);
 int op_sub (int a, int b);
