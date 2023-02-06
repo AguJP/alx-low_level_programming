43848db5c498f6b757ae6c8a16873e71cc89a11d
AguJP <agujpa@gmail.com>
2023-02-06 14:13:34 -0800

    Read textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * read_textfile - Function to read a textfile and print to POSIX stdout
+ * @filename: name of file to be read
+ * @letters: no. of letters to read and print
+ * Return: no. of letters read and printed if successful, else 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *buffer;
+
+	if (!filename)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(STDIN_FILENO, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (w);
+}
