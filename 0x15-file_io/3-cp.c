41585f46905f1fba16cd409f42c84690e2226880
AguJP <agujpa@gmail.com>
2023-02-06 15:49:27 -0800

    Copy file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -54,7 +54,7 @@ void close_file(int fd)
 
 int main(int argc, char *argv[])
 {
-	int from, to , r, w;
+	int from, to, r, w;
 	char *buffer;
 
 	if (argc != 3)
@@ -66,7 +66,7 @@ int main(int argc, char *argv[])
 	buffer = create_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
 	r = read(from, buffer, 1024);
-	to = open(argvv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
 		if (from == -1 || r == -1)
