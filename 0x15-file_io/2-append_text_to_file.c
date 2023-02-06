82bfc6619c6105086a3db16984a5dd9d67d95c8f
AguJP <agujpa@gmail.com>
2023-02-06 15:05:51 -0800

    Append text_to_file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	int o, w, len = 0;
 
 	if (!filename)
-		return (0);
+		return (-1);
 
 	if (text_content)
 	{
@@ -28,5 +28,5 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	close(o);
 
-	return (-1);
+	return (1);
 }
