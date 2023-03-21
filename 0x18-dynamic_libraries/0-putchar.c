fb9e9c25f487bad4128e2e6e39d81a9bf816c48c
AguJP <agujpa@gmail.com>
2023-03-20 18:20:55 -0700

    Dynamic libraries

--- 0x18-dynamic_libraries/0-putchar.c
+++ 0x18-dynamic_libraries/0-putchar.c
@@ -1,19 +1,19 @@
 #include "main.h"
+#include <stdio.h>
+#include <unistd.h> // for the write() function
+
 /**
- * main - Entry point
+ * _putchar - Function to write to stdout
+ * @c: character to write
  * Return: Always 0
  */
 
-int main(void)
+int _putchar(char c)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+    if (write(STDOUT_FILENO, &c, 1) != 1)
+    { // write the character to stdout using write(), checking for errors
+        return EOF; // return EOF to indicate failure if an error occurred
+    }
+    return (unsigned char) c;
+    // return the character as an unsigned char cast to an int to indicate success
 }
