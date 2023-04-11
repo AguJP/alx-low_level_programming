3ced058d92d12dcee45e52f280a2e0becbeb758d
AguJP <agujpa@gmail.com>
2023-04-11 00:31:30 -0700

    Key index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,11 @@
+/**
+ * key_index - Function to return the index of a hashed key using hash_djb2 hash
+ * @key: The key
+ * @size: Size of the array of the hash table
+ * Return: The index where the key/value pair is stored in hashtable array
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
